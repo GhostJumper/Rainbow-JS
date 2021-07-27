bb22b4ec8ba45d0311e7969142148d00b939ac20
Unrea1 <ghostjumperhd@gmail.com>
2021-07-27 17:24:35 +0200

    fixed all Algorithms

--- hashing/hash_handler.js
+++ hashing/hash_handler.js
@@ -2,65 +2,37 @@ const db = require("../db/maria_db")
 const hash = require("./hashing")
 const e = require("express");
 
-async function manageBatch(batch) {
-    if (typeof batch.req_list === 'undefined') throw Error(`couldn't find req_list`)
-
+async function manageBatch(req_body) {
+    if (typeof req_body.req_list === 'undefined') throw Error(`couldn't find req_list`)
     let result = {"res_list": [], "err_list": []}
 
-    for (const entry of batch.req_list) {
-        if (typeof entry.plain_text !== 'undefined' && typeof entry.algorithms !== 'undefined') {
-            result.res_list.push(await createBatchEntry(entry.plain_text, entry.algorithms))
-        } else
-            result.err_list.push(entry)
+    for (const entry of req_body.req_list) {
+        result = await manageOne(entry, result)
     }
 
     return result
-
 }
 
-async function createBatchEntry(plain_text, algorithms) {
-    return {
-        "plain_text": plain_text,
-        "res": filterResultByAlgorithms(await hashOneToDB(plain_text), algorithms)
-    }
+async function manageOne(req_body_entry, result) {
+
+    if (typeof req_body_entry.plain_text !== "undefined" && typeof req_body_entry.algorithms !== "undefined") {
+        result.res_list.push(filterAlgorithms(await hashOneToDB(req_body_entry.plain_text), req_body_entry.algorithms))
+    } else result.err_list.push(req_body_entry)
+
+    return result
 }
 
-//Takes one String, hashes it, and returns the result of one or all algorithms
-async function manageOne(plain_text, algorithm) {
-
-    let hashes = await hashOneToDB(plain_text)
-
-    if (typeof algorithm === 'undefined') {
-        return hashes
-    } else {
-        return filterResultByAlgorithm(hashes, algorithm)
-    }
-
-}
-
-//Takes a list of hashes and filters by one algorithm
-function filterResultByAlgorithm(hashes, algorithm) {
-    if (!(algorithm in hash.known_algorithms)) return {[algorithm]: null}
-    return {[algorithm]: hashes[algorithm]}
-}
-
-//Takes a list of hashes and filters by multiple algorithms
-function filterResultByAlgorithms(hashes, algorithms) {
-    if (algorithms.length === 0) return removePlainText(hashes)
-
-    let result = {}
+function filterAlgorithms(hashes, algorithms) {
+    let result = {"plain_text": hashes.plain_text}
+    if (algorithms.length === 0) return hashes
     algorithms.forEach(algorithm => {
-        result[algorithm] = filterResultByAlgorithm(hashes, algorithm)[algorithm]
+        if (typeof hashes[algorithm] === "undefined") {
+            result[algorithm] = null
+        } else result[algorithm] = hashes[algorithm]
     })
 
     return result
-}
 
-//Removes the "plain_text" from the hash object
-function removePlainText(hashes) {
-    let result = hashes
-    delete result.plain_text
-    return result
 }
 
 //Takes a String, looks up if already in db, if not hashes, inserts and returns all results
@@ -73,4 +45,4 @@ async function hashOneToDB(plain_text) {
     return result
 }
 
-module.exports = {manageOne, manageBatch}
+module.exports = {manageBatch}
--- routes/create_hash.js
+++ routes/create_hash.js
@@ -6,19 +6,7 @@ let hash_handler = require("../hashing/hash_handler")
 router.use(express.json())
 
 router.get("/", async (req, res) => {
-
     try {
-        res.send(await hash_handler.manageOne(req.query.plain_text, req.query.algorithm))
-    } catch (err) {
-        res.status(400).send(err.message)
-    }
-
-})
-
-
-router.get("/batch", async (req, res) => {
-    try {
-
         res.send(await hash_handler.manageBatch(req.body))
     } catch (err) {
         res.status(400).send(err.message)
